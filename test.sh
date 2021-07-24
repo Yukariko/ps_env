4a4d50c55bc2b76b4c25983f92cac380fec9a7eb
yukariko <rkdals7979@gmail.com>
2021-07-25 07:17:32 +0900

    print before run test

--- test.sh
+++ test.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
-make > /dev/null
+make 2> output.txt
 [ $? -eq 0 ] || exit 1
 
+echo -n "" > output.txt
 while read line
 do
     echo "$line" >> tmp.txt
     if [ "$line" = "" ]; then
-        ./test < tmp.txt
-        echo "--------"
+        echo "do test..."
+        ./test < tmp.txt >> output.txt
+        echo "--------" >> output.txt
         rm tmp.txt
     fi
 done < input.txt
 
-[ -e tmp.txt ] && ( ./test < tmp.txt ; rm tmp.txt ; echo "--------" )
+[ -e tmp.txt ] && ( ./test < tmp.txt >> output.txt; rm tmp.txt ; echo "--------" >> output.txt)
