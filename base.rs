90038ee0c6fa2b2493e9e5f47575c9959eaa2be4
yukariko <yukariko@github.com>
2021-10-16 01:35:36 +0900

    update base code

--- base.rs
+++ base.rs
@@ -1,4 +1,23 @@
 use std::io;
+use std::cmp;
+
+fn read_integer() -> i32 {
+    let mut s = String::new();
+    io::stdin().read_line(&mut s).unwrap();
+    let values : i32 = s.trim().parse().expect("");
+    values
+}
+
+fn read_integers() -> Vec<i32> {
+    let mut s = String::new();
+    io::stdin().read_line(&mut s).unwrap();
+    let values:Vec<i32> = s
+        .split_whitespace()
+        .map(|s| s.parse().unwrap())
+        .collect();
+
+    values
+}
 
 fn main() {
 
